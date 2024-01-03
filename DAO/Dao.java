1bc82ee2a0b4c423c64cac1d8dfc49ea3064ba09
Romain Trochu <romain.trochu@gmail.com>
2024-01-03 15:22:32 +0100

    re-init projet

---
+++ DAO/Dao.java
@@ -0,0 +1,37 @@
+package DAO;
+
+import java.sql.*;
+
+public class Dao {
+    
+    private Dao() {
+    }
+
+    public static Connection connectDatabase() {
+        try {
+            // driver class et obj connexion
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc?useSSL=false", "root", "");
+            return conn;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("ERREUR : " + e);
+            return null;
+        }
+    }
+    
+    public static void closeResources(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("ERREUR : " + e);
+        }
+    }
+}
---
+++ Main/Main.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+import Main.MenuClient;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Opérations sur Utilisateurs");
+            System.out.println("2. Opérations sur Clients");
+            System.out.println("3. Opérations sur Fournisseurs");
+            System.out.println("4. Opérations sur Articles");
+            System.out.println("0. Quitter");
+            System.out.print("Choisissez la table (0-4): ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Pour consommer la nouvelle ligne
+
+            switch (choice) {
+                case 1:
+                    // À implémenter : runUser();
+                    break;
+                case 2:
+                    MenuClient.run();         
+                    break;
+                case 3:
+//                    MenuFournisseurs.run();
+                    break;
+                case 4:
+//                    MenuArticles.run();
+                    break;
+                case 0:
+                    System.out.println("Au revoir !");
+                    System.exit(0);
+                default:
+                    System.out.println("Choix invalide. Veuillez réessayer.");
+            }
+        }
+    }
+}
